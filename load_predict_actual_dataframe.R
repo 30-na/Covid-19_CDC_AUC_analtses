a49516a8157d653306baa0d748f61a96643d3922
sina mokhtar <sinamokhtar@tamu.edu>
2023-01-30 21:44:32 -0700

    make a loop to save prediction result for a week

--- load_predict_actual_dataframe.R
+++ load_predict_actual_dataframe.R
@@ -3,15 +3,19 @@ library(dplyr)
 library(tidyr)
 library(usdata)
 library(data.table)
-
+library(pROC)
+library(ggplot2)
+library(plotROC)
 
 # load CDC community level by county
-community.level.county.file = fread("rawData/United_States_COVID-19_Community_Levels_by_County.csv")
-names(community.level.county.file)
+community_level_county_file = fread("rawData/United_States_COVID-19_Community_Levels_by_County.csv")
     
-community.lvl = community.level.county.file %>%
+community_lvl_data = community_level_county_file %>%
     rename("community_level" = "covid-19_community_level") %>%
     mutate(community_level = tolower(community_level),
+           community_level = case_when(community_level == "low"  ~ 1,
+                                       community_level == "medium"  ~ 2,
+                                       community_level == "high"  ~ 3),
            date_updated = as.Date(date_updated, format="%Y/%m/%d"),
            stateFips = paste(state, county_fips, sep = ",")) %>%
     group_by(stateFips) %>%
@@ -21,92 +25,89 @@ community.lvl = community.level.county.file %>%
            next_3week_inpatient_bed = lead(x = covid_inpatient_bed_utilization, n = 3L))
     
 
-names(community.lvl)    
+
 # compare each county with others in different community level
 
 i = 1
-j = 1
-weekdays = sort(unique(community.lvl$date_updated))
+weekdays = sort(unique(community_lvl_data$date_updated))
 
-counties.list = unique(dplyr::filter(community.lvl, date_updated == weekdays[i])$stateFips)
+target_counties_list = unique(dplyr::filter(community_lvl_data, date_updated == weekdays[i])$stateFips)
 
-target.CL = dplyr::filter(community.lvl, date_updated == weekdays[i] &
-                              stateFips == counties.list[j])$community_level
+# make and empty data Frame
+columnNames = c("target_stateFips",
+                "target_community_level",
+                "target_covid_inpatient_bed_utilization",  
+                "target_covid_hospital_admissions_per_100k",
+                "target_covid_cases_per_100k",
+                "target_next_3week_hospital_admission",
+                "target_next_3week_inpatient_bed",
+                "compared_stateFips",
+                "compared_community_level",              
+                "compared_next_3week_hospital_admission",
+                "compared_next_3week_inpatient_bed",
+                "predict_severe_outcome",                
+                "actual_severe_hospital_admission",
+                "actual_severe_inpatient_bed")
+predict_data = data.frame(matrix(nrow = 0, ncol=length(columnNames)))
+colnames(predict_data) = columnNames
 
-target.hospital.admission = dplyr::filter(community.lvl, date_updated == weekdays[i] &
-                              stateFips == counties.list[j])$covid_hospital_admissions_per_100k
-
-target.inpatient.bed = dplyr::filter(community.lvl, date_updated == weekdays[i] &
-                              stateFips == counties.list[j])$covid_inpatient_bed_utilization
-
-target.next3week.hospital.admission = dplyr::filter(community.lvl, date_updated == weekdays[i] &
-                              stateFips == counties.list[j])$next_3week_hospital_admission
-
-target.next3week.inpatient.bed = dplyr::filter(community.lvl, date_updated == weekdays[i] &
-                              stateFips == counties.list[j])$next_3week_inpatient_bed
-
-camparable.counties.list = unique(dplyr::filter(community.lvl, date_updated == weekdays[i] &
-                                                    community_level != target.CL)$stateFips)
+names(community_lvl_data)
+for(j in 1:length(target_counties_list)){
+    target_county = community_lvl_data %>%
+        filter(date_updated == weekdays[i],
+               stateFips == target_counties_list[j]) %>%
+        select(target_stateFips = stateFips,
+               target_community_level = community_level,
+               target_covid_inpatient_bed_utilization = covid_inpatient_bed_utilization,
+               target_covid_hospital_admissions_per_100k = covid_hospital_admissions_per_100k,
+               target_covid_cases_per_100k = covid_cases_per_100k,
+               target_next_3week_hospital_admission = next_3week_hospital_admission,
+               target_next_3week_inpatient_bed = next_3week_inpatient_bed)
     
-
-data = data.frame(date = weekdays[i],
-                  county = counties.list[j],
-                  community.level = target.CL,
-                  hospital.admission = target.hospital.admission,
-                  inpatient.bed = target.inpatient.bed,
-                  next3week.hospital.admission = target.next3week.hospital.admission,
-                  next3week.inpatient.bed = target.next3week.inpatient.bed)
-
-
-
-
-
-
-  
-#weekdays = unique(community.lvl$date_updated)  
-weekdays = c("2022-08-11")
-
-# date loop
-for(i in 1:length(weekdays)){
+    
+    compared_counties = community_lvl_data %>%
+        filter(date_updated == weekdays[i],
+               community_level != target_county$target_community_level) %>%
+        select(compared_stateFips = stateFips,
+               compared_community_level = community_level,
+               #compared_covid_inpatient_bed_utilization = covid_inpatient_bed_utilization,
+               #compared_covid_hospital_admissions_per_100k = covid_hospital_admissions_per_100k,
+               compared_next_3week_hospital_admission = next_3week_hospital_admission,
+               compared_next_3week_inpatient_bed = next_3week_inpatient_bed)
+    
+    data = target_county %>%
+        cbind(compared_counties) %>%
+        mutate(predict_severe_outcome = ifelse(target_community_level > compared_community_level, 1, 0),
+               actual_severe_hospital_admission = ifelse(target_next_3week_hospital_admission > compared_next_3week_hospital_admission, 1, 0),
+               actual_severe_inpatient_bed = ifelse(target_next_3week_inpatient_bed > compared_next_3week_inpatient_bed, 1, 0))
+    predict_data = rbind(predict_data, data)
+    print(j)
+    
 }
-
-CL.week = community.lvl %>%
-    filter(date_updated == "2022-08-11")
-
-
-# get the list of all counties with different community level that should be compared
-getComparableCountiesList = function(data, target.county){
-    # get the target county community level
-    county.CL = dplyr::filter(data = data, stateFips == target.county)$community_level
-        
-    # filter the counties that have different comunity level
-    df = df %>%
-        filter(community_level != county.CL)
-    
-    comparable.counties.list = unique(df$stateFips)
-    return(comparable.counties.list)
-}
+save(predict_data, file="ProcessedData/predict_data.RDA")
+library(pROC)
+par(pty = "s")
+roc(predict_data$predict_severe_outcome,
+    predict_data$actual_severe_hospital_admission,
+    plot=TRUE,
+    legacy.axes=TRUE,
+    percent=TRUE,
+    #xlab="False Positive Percentage",
+    #ylab="True Postive Percentage",
+    col="#377eb8",
+    lwd=4,
+    print.auc=TRUE)
 
 
+roc(predict_data$predict_severe_outcome,
+         predict_data$actual_severe_inpatient_bed,
+         plot=TRUE,
+         col="#4daf4a",
+         lwd=4,
+         print.auc=TRUE)
+legend("bottomright", legend=c("Hospital Admission", "Impatient Bed"), col=c("#377eb8", "#4daf4a"), lwd=4)
 
-
-target.county = CL.week$stateFips[1]
-
-comparable.counties.list = getComparableCountiesList(CL.target.week = CL.week,
-                                                     target.county = target.county)
-
-CL.week = community.lvl %>%
-    filter(date_updated == weekdays[i])
-
-
-community_level_stateFips = community_level_LMH %>%
-    mutate(stateFips = paste(state, fips_code, sep = ","))
-
-stateFips.list = unique(CL.week$stateFips)
-stateFips.list = unique(CL.week$stateFips)
-
-# full list of counties and dates
-full_county_date = data.frame(stateFips = rep(stateFips_list, each = length(days)),
-                              date = rep(days, times = length(stateFips_list)))
-
-    
+a = predict_data %>%
+    mutate(community_level_threshold = case_when())
+mean(a$check, na.rm = T)
+?roc
