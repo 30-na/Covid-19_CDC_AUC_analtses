6faec1e16fa923f97e49a7db2d219a4f3f231144
sina mokhtar <sinamokhtar@gmail.com>
2023-02-03 12:48:48 -0700

    make a for loop[ to save data weekly

--- load_predict_actual_dataframe.R
+++ load_predict_actual_dataframe.R
@@ -1,11 +1,8 @@
 # load library
 library(dplyr)
 library(tidyr)
-library(usdata)
 library(data.table)
-library(pROC)
-library(ggplot2)
-library(plotROC)
+
 
 # load CDC community level by county
 community_level_county_file = fread("rawData/United_States_COVID-19_Community_Levels_by_County.csv")
@@ -28,11 +25,6 @@ community_lvl_data = community_level_county_file %>%
 
 # compare each county with others in different community level
 
-i = 1
-weekdays = sort(unique(community_lvl_data$date_updated))
-
-target_counties_list = unique(dplyr::filter(community_lvl_data, date_updated == weekdays[i])$stateFips)
-
 # make and empty data Frame
 columnNames = c("target_stateFips",
                 "target_community_level",
@@ -48,66 +40,55 @@ columnNames = c("target_stateFips",
                 "predict_severe_outcome",                
                 "actual_severe_hospital_admission",
                 "actual_severe_inpatient_bed")
-predict_data = data.frame(matrix(nrow = 0, ncol=length(columnNames)))
-colnames(predict_data) = columnNames
 
-names(community_lvl_data)
-for(j in 1:length(target_counties_list)){
-    target_county = community_lvl_data %>%
-        filter(date_updated == weekdays[i],
-               stateFips == target_counties_list[j]) %>%
-        select(target_stateFips = stateFips,
-               target_community_level = community_level,
-               target_covid_inpatient_bed_utilization = covid_inpatient_bed_utilization,
-               target_covid_hospital_admissions_per_100k = covid_hospital_admissions_per_100k,
-               target_covid_cases_per_100k = covid_cases_per_100k,
-               target_next_3week_hospital_admission = next_3week_hospital_admission,
-               target_next_3week_inpatient_bed = next_3week_inpatient_bed)
+
+rbind_data = data.frame(matrix(nrow = 0, ncol=length(columnNames)))
+colnames(rbind_data) = columnNames 
+
+weekdays = sort(unique(community_lvl_data$date_updated))
+weekIndex = seq(1, length(weekdays)-3, by=3)
+for(i in weekIndex){
     
+    week_i_Data = dplyr::filter(community_lvl_data,
+                                date_updated == weekdays[i])
     
-    compared_counties = community_lvl_data %>%
-        filter(date_updated == weekdays[i],
-               community_level != target_county$target_community_level) %>%
-        select(compared_stateFips = stateFips,
-               compared_community_level = community_level,
-               #compared_covid_inpatient_bed_utilization = covid_inpatient_bed_utilization,
-               #compared_covid_hospital_admissions_per_100k = covid_hospital_admissions_per_100k,
-               compared_next_3week_hospital_admission = next_3week_hospital_admission,
-               compared_next_3week_inpatient_bed = next_3week_inpatient_bed)
+    rbind_data_week = data.frame(matrix(nrow = 0, ncol=length(columnNames)))
+    colnames(rbind_data_week) = columnNames
     
-    data = target_county %>%
-        cbind(compared_counties) %>%
-        mutate(predict_severe_outcome = ifelse(target_community_level > compared_community_level, 1, 0),
-               actual_severe_hospital_admission = ifelse(target_next_3week_hospital_admission > compared_next_3week_hospital_admission, 1, 0),
-               actual_severe_inpatient_bed = ifelse(target_next_3week_inpatient_bed > compared_next_3week_inpatient_bed, 1, 0))
-    predict_data = rbind(predict_data, data)
-    print(j)
+    target_counties_list = week_i_Data$stateFips
+    target_counties_size = length(target_counties_list)
     
+    for(j in 1:target_counties_size){
+        target_county = week_i_Data %>%
+            dplyr::filter(stateFips == target_counties_list[j]) %>%
+            select(date = date_updated,
+                   target_stateFips = stateFips,
+                   target_community_level = community_level,
+                   target_covid_inpatient_bed_utilization = covid_inpatient_bed_utilization,
+                   target_covid_hospital_admissions_per_100k = covid_hospital_admissions_per_100k,
+                   target_covid_cases_per_100k = covid_cases_per_100k,
+                   target_next_3week_hospital_admission = next_3week_hospital_admission,
+                   target_next_3week_inpatient_bed = next_3week_inpatient_bed)
+       
+        
+        compared_counties = week_i_Data[j:target_counties_size,] %>%
+            dplyr::filter(community_level != target_county$target_community_level) %>%
+            select(compared_stateFips = stateFips,
+                   compared_community_level = community_level,
+                   #compared_covid_inpatient_bed_utilization = covid_inpatient_bed_utilization,
+                   #compared_covid_hospital_admissions_per_100k = covid_hospital_admissions_per_100k,
+                   compared_next_3week_hospital_admission = next_3week_hospital_admission,
+                   compared_next_3week_inpatient_bed = next_3week_inpatient_bed)
+        
+        cbind_data = cbind(target_county, compared_counties)
+        
+        
+        rbind_data_week = rbind(rbind_data_week, cbind_data)
+        print(paste(i, j, sep=", "))
+
+                     
+    }
+    
+    save(rbind_data_week, file=paste("ProcessedData/rbind_data_week", i, ".RDA", sep=""))
+    print(paste("file week" , i,  " saved !!!"))
 }
-save(predict_data, file="ProcessedData/predict_data.RDA")
-library(pROC)
-par(pty = "s")
-roc(predict_data$predict_severe_outcome,
-    predict_data$actual_severe_hospital_admission,
-    plot=TRUE,
-    legacy.axes=TRUE,
-    percent=TRUE,
-    #xlab="False Positive Percentage",
-    #ylab="True Postive Percentage",
-    col="#377eb8",
-    lwd=4,
-    print.auc=TRUE)
-
-
-roc(predict_data$predict_severe_outcome,
-         predict_data$actual_severe_inpatient_bed,
-         plot=TRUE,
-         col="#4daf4a",
-         lwd=4,
-         print.auc=TRUE)
-legend("bottomright", legend=c("Hospital Admission", "Impatient Bed"), col=c("#377eb8", "#4daf4a"), lwd=4)
-
-a = predict_data %>%
-    mutate(community_level_threshold = case_when())
-mean(a$check, na.rm = T)
-?roc
